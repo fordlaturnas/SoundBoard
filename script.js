508917a5e6c4de65de7601116d5d552490318fc3
Ford Laturnas <fordlaturnas1209@gmail.com>
2021-11-23 19:03:33 +0800

    fix button

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const sounds = [
-    'laugh', 
-    'mario',
-    'back',
-    'spongebob', 
-    'suspense',
-    'wii',
-    'rick'
-]
+  'laugh',
+  'mario',
+  'back..',
+  'boby.',
+  'thrill...',
+  'wiiiii...',
+  'roll.....',
+];
 
 // loop to this sounds
-sounds.forEach(sound => {
-    //create button
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+sounds.forEach((sound) => {
+  //create button
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
 
-    btn.innerText = sound
+  btn.innerText = sound;
 
-    // loop to each button we add even listener on to each one
-    btn.addEventListener('click', () =>{
-        stopSongs()
+  // loop to each button we add even listener on to each one
+  btn.addEventListener('click', () => {
+    stopSongs();
 
-        document.getElementById(sound).play()
-    } )
+    document.getElementById(sound).play();
+  });
 
-    document.getElementById('buttons').appendChild(btn) 
-})
+  document.getElementById('buttons').appendChild(btn);
+});
 
 function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
-        song.pause()
-        song.currentTime = 0
-    })
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
 }
